66d830dc39fa0398b91e7b75ef1250792dfd88ff
pwp114514 <117086223+pwp114514@users.noreply.github.com>
2023-02-19 15:44:30 +0800

    Update GameJolt.hx

--- source/GameJolt.hx
+++ source/GameJolt.hx
@@ -21,7 +21,7 @@ import openfl.text.TextFormat;
 import tentools.api.FlxGameJolt as GJApi;
 
 using StringTools;
-class GameJoltAPI extends MusicBeatState
+class GameJoltAPI
 {
 	static var userLogin:Bool = false;
 	public static var totalTrophies:Float = GJApi.TROPHIES_ACHIEVED + GJApi.TROPHIES_MISSING;
